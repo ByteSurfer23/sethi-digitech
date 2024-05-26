f46837c3a11897ae1efa30d1a78eaa091819f841
Jabez <jabezthedeveloper@gmail.com>
2024-05-26 22:35:05 +0530

    first commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+    title:'To-Do-List',
+    imgUrl:'/assets/to-do-list.png',
+    stack:['MongoDB','Express.js','React.js','Node.js'],
+    link:'#'
+},
+    {
+    title:'About-Us page',
+    imgUrl:'/assets/abt_us.png',
+    stack:['HTML','CSS','Javascript'],
+    link:'#'
+},
+    {
+    title:'Contact-Us form',
+    imgUrl:'/assets/cont_us.png',
+    stack:['HTML','CSS','JavaScript'],
+    link:'#'
+},
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        year:'Today',
+        title:'Aspiring Developer at VIT',
+        duration:'',
+        details:"I'm currently pursuing Bachelors in Computer Science Engineering in Vellore Institute of Technology,I joined the institute in the year 2022.The institute has been very influential in cultivating an interest for Web development in me.",
+    },
+    {
+        year:'2022',
+        title:'High Schooling',
+        duration:'',
+        details:"I completed my high schooling from St.Michael's Academy in the year 2022, I passed out with a final score of 90.6%.",
+    },
+    {
+        year:'2020',
+        title:'Middle School',
+        duration:'',
+        details:"I completed my high schooling from St.Michael's Academy in the year 2022, I passed out with a final score of 96.4%.",
+    }
+]
